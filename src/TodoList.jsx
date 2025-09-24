83357600a9862108307e8105b5a8b1581553155c
dv-jamie <sjm2506@gmail.com>
2025-09-24 09:59:04 +0900

    TodoList 조건부 렌더링 설정

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,19 @@
 function TodoList({ todos, deleteTodo }) {
     return (
         <>
-            {todos.map((todo, i) => {
-                return (
-                    <li key={i} className='todo-item'>
-                    <input type='checkbox' />
-                    <p>{todo}</p>
-                    <button
-                        className='delete-button'
-                        onClick={() => deleteTodo(i)}
-                    >X</button>
-                    </li>
-                )
+            {todos.length === 0
+                ? <li className="todo-item">할 일이 없습니다...</li>
+                : todos.map((todo, i) => {
+                    return (
+                        <li key={i} className='todo-item'>
+                        <input type='checkbox' />
+                        <p>{todo}</p>
+                        <button
+                            className='delete-button'
+                            onClick={() => deleteTodo(i)}
+                        >X</button>
+                        </li>
+                    )
             })}
         </>
     )
